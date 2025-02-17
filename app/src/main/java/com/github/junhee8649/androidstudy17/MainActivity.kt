6c60146ca1d53b9dd4b95f2893285e09c7d7a742
Junhee-lee <sk5514224@gmail.com>
2025-02-17 15:35:33 +0900

    [feat] week3 WoofApp

--- app/src/main/java/com/github/junhee8649/androidstudy17/MainActivity.kt
+++ app/src/main/java/com/github/junhee8649/androidstudy17/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.github.junhee8649.androidstudy17.week1.ComposeQuadrantApp
-import com.github.junhee8649.androidstudy17.week3.AffirmationsApp.AffirmationsApp
+import com.github.junhee8649.androidstudy17.week3.WoofApp.WoofApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +18,7 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                AffirmationsApp()
+                WoofApp()
             }
         }
     }
