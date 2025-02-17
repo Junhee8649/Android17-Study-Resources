533f1ce6ba3897575d5a6fd3d7b7e23538713779
Junhee-lee <sk5514224@gmail.com>
2025-02-17 17:10:40 +0900

    [feat] week4 dessertclicker

--- app/src/main/java/com/github/junhee8649/androidstudy17/MainActivity.kt
+++ app/src/main/java/com/github/junhee8649/androidstudy17/MainActivity.kt
@@ -9,20 +9,20 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.github.junhee8649.androidstudy17.week3.WoofApp.WoofApp
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            // A surface container using the 'background' color from the theme
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                WoofApp()
-            }
-        }
-    }
-}
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            // A surface container using the 'background' color from the theme
+//            Surface(
+//                modifier = Modifier.fillMaxSize(),
+//                color = MaterialTheme.colorScheme.background
+//            ) {
+//                WoofApp()
+//            }
+//        }
+//    }
+//}
 
 
 
