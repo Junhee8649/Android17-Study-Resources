b98c462d06af84bf0143db9817b8af02fb6356c3
Junhee-lee <sk5514224@gmail.com>
2025-02-27 16:35:21 +0900

    Login Only

--- app/src/main/java/com/github/junhee8649/androidstudy17/week3/AffirmationsApp/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package com.github.junhee8649.androidstudy17.week3.AffirmationsApp.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmation(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
-)
--- app/src/main/java/com/github/junhee8649/androidstudy17/week4/Dessert/JustDessert/model/Dessert.kt
+++
@@ -1,3 +0,0 @@
-package com.github.junhee8649.androidstudy17.week4.Dessert.JustDessert.model
-
-//data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
